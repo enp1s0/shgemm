1ca3cc09a64b39a6318c58f0046efcbb0cfa81f3
mutsuki <mutsuki@momo86.net>
2022-02-01 16:25:32 +0900

    Add calling shgemm_kernel_launcher for NN

--- src/shgemm.cu
+++ src/shgemm.cu
@@ -258,5 +258,15 @@ void mtk::shgemm::shgemm(
 				beta_ptr,
 				c_ptr, ldc
 				);
+	} else if (op_a == mtk::shgemm::op_n && op_b == mtk::shgemm::op_n) {
+		shgemm_kernel_launcher<mtk::shgemm::op_n, mtk::shgemm::op_n>(
+				handle,
+				m, n, k,
+				alpha_ptr,
+				a_ptr, lda,
+				b_ptr, ldb,
+				beta_ptr,
+				c_ptr, ldc
+				);
 	}
 }
