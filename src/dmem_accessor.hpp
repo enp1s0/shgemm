687fad23c4fbcf4d154e41772f3914fa73935e01
mutsuki <mutsuki@momo86.net>
2022-02-22 22:52:06 +0900

    Revert "Merge branch '12-reduce-bank-conflict' into 'main'"
    This reverts merge request !13

--- src/dmem_accessor.hpp
+++ src/dmem_accessor.hpp
@@ -28,10 +28,9 @@ struct dmem_loader_n {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::A_smem_skew);
 
 					// 128 bit memory access
-					cutf::cp_async::cp_async<16>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<16>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			} else if ((ldd & 0x1) == 0) {
 				for (unsigned i_offset = 0; i_offset < SMEM_M * SMEM_N; i_offset += BLOCK_SIZE * 2) {
@@ -39,10 +38,9 @@ struct dmem_loader_n {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::A_smem_skew);
 
 					// 64 bit memory access
-					cutf::cp_async::cp_async<8>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<8>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			} else {
 				for (unsigned i_offset = 0; i_offset < SMEM_M * SMEM_N; i_offset += BLOCK_SIZE) {
@@ -50,9 +48,8 @@ struct dmem_loader_n {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::A_smem_skew);
 
-					cutf::cp_async::cp_async<4>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<4>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			}
 		} else {
@@ -61,12 +58,11 @@ struct dmem_loader_n {
 				const auto m = (i % SMEM_M) + dmem_start_m;
 				const auto n = (i / SMEM_M) + dmem_start_n;
 				const auto dmem_index = m + n * ldd;
-				const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::A_smem_skew);
 
 				if (m < dmem_size_m && n < dmem_size_n) {
-					cutf::cp_async::cp_async<4>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<4>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				} else {
-					smem_ptr[smem_index] = 0.f;
+					smem_ptr[i] = 0.f;
 				}
 			}
 		}
@@ -90,9 +86,8 @@ struct dmem_loader_n<half, SMEM_M, SMEM_N, BLOCK_SIZE> {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::B_smem_skew);
 
-					cutf::cp_async::cp_async<16>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<16>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			} else if ((ldd & 0x3) == 0) {
 				for (unsigned i_offset = 0; i_offset < SMEM_M * SMEM_N; i_offset += BLOCK_SIZE * 4) {
@@ -100,9 +95,8 @@ struct dmem_loader_n<half, SMEM_M, SMEM_N, BLOCK_SIZE> {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::B_smem_skew);
 
-					cutf::cp_async::cp_async<8>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<8>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			} else if ((ldd & 0x1) == 0) {
 				for (unsigned i_offset = 0; i_offset < SMEM_M * SMEM_N; i_offset += BLOCK_SIZE * 2) {
@@ -110,9 +104,8 @@ struct dmem_loader_n<half, SMEM_M, SMEM_N, BLOCK_SIZE> {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::B_smem_skew);
 
-					cutf::cp_async::cp_async<4>(&smem_ptr[smem_index], &dmem_ptr[dmem_index]);
+					cutf::cp_async::cp_async<4>(&smem_ptr[i], &dmem_ptr[dmem_index]);
 				}
 			} else {
 				for (unsigned i_offset = 0; i_offset < SMEM_M * SMEM_N; i_offset += BLOCK_SIZE) {
@@ -120,9 +113,8 @@ struct dmem_loader_n<half, SMEM_M, SMEM_N, BLOCK_SIZE> {
 					const auto m = (i % SMEM_M) + dmem_start_m;
 					const auto n = (i / SMEM_M) + dmem_start_n;
 					const auto dmem_index = m + n * ldd;
-					const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::B_smem_skew);
 
-					smem_ptr[smem_index] = dmem_ptr[dmem_index];
+					smem_ptr[i] = dmem_ptr[dmem_index];
 				}
 			}
 		} else {
@@ -131,14 +123,13 @@ struct dmem_loader_n<half, SMEM_M, SMEM_N, BLOCK_SIZE> {
 				const auto m = (i % SMEM_M) + dmem_start_m;
 				const auto n = (i / SMEM_M) + dmem_start_n;
 				const auto dmem_index = m + n * ldd;
-				const auto smem_index = (i % SMEM_M) + (i / SMEM_M) * (SMEM_M + mtk::shgemm::device::B_smem_skew);
 
 				auto v = cutf::type::cast<half>(0);
 				if (m < dmem_size_m && n < dmem_size_n) {
 					v = dmem_ptr[dmem_index];
 				}
 
-				smem_ptr[smem_index] = v;
+				smem_ptr[i] = v;
 			}
 		}
 		cutf::cp_async::commit();
--- src/utils.hpp
+++ src/utils.hpp
@@ -15,24 +15,6 @@ template <class TC_T>
 using A_Policy = typename mtk::wmma::tcec::detail::default_policy<TC_T, mtk::wmma::tcec::op_with_error_correction   , mtk::wmma::tcec::op_mma>::type;
 template <class TC_T>
 using B_Policy = typename mtk::wmma::tcec::detail::default_policy<TC_T, mtk::wmma::tcec::op_without_error_correction, mtk::wmma::tcec::op_mma>::type;
-
-// CAUTION: The skew is not suoorted yet
-constexpr unsigned A_smem_skew = 0;
-constexpr unsigned B_smem_skew = 0;
-
-template <unsigned SMEM_M, unsigned SMEM_K, class Layout>
-struct get_A_smem_size{const static unsigned value = 0;};
-template <unsigned SMEM_M, unsigned SMEM_K>
-struct get_A_smem_size<SMEM_M, SMEM_K, mtk::shgemm::utils::row_major> {const static unsigned value = (SMEM_K + mtk::shgemm::device::A_smem_skew) * SMEM_M;};
-template <unsigned SMEM_M, unsigned SMEM_K>
-struct get_A_smem_size<SMEM_M, SMEM_K, mtk::shgemm::utils::col_major> {const static unsigned value = (SMEM_M + mtk::shgemm::device::A_smem_skew) * SMEM_K;};
-
-template <unsigned SMEM_K, unsigned SMEM_N, class Layout>
-struct get_B_smem_size{const static unsigned value = 0;};
-template <unsigned SMEM_K, unsigned SMEM_N>
-struct get_B_smem_size<SMEM_K, SMEM_N, mtk::shgemm::utils::row_major> {const static unsigned value = (SMEM_N + mtk::shgemm::device::A_smem_skew) * SMEM_K;};
-template <unsigned SMEM_K, unsigned SMEM_N>
-struct get_B_smem_size<SMEM_K, SMEM_N, mtk::shgemm::utils::col_major> {const static unsigned value = (SMEM_K + mtk::shgemm::device::A_smem_skew) * SMEM_N;};
 } // namespace device
 } // namespace shgemm
 } // namespace mtk
