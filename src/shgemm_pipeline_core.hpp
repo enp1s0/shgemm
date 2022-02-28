2e59fa8a48e80b728f63e9e3803f1f9a79acfdf6
mutsuki <mutsuki@momo86.net>
2022-02-28 13:37:11 +0900

    Fix waiting for async_cp

--- src/shgemm_pipeline_core.hpp
+++ src/shgemm_pipeline_core.hpp
@@ -111,7 +111,7 @@ struct shgemm_pipeline_core<
 				a_smem_ptr + (1 - ((block_k / SMEM_K) & 0x1)) * SMEM_K * SMEM_M,
 				b_smem_ptr + (1 - ((block_k / SMEM_K) & 0x1)) * SMEM_K * SMEM_N
 				);
-		cutf::cp_async::wait_group<2>();
+		cutf::cp_async::wait_all();
 		__syncthreads();
 	}
 
